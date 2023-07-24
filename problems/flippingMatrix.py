24767e2323e15eb90e442676e20c1dff0d3024c0
Layla Hashmi <laylahashmi@gmail.com>
2023-07-24 18:56:56 -0500

    completed in O(n^2) complexity

---
+++ problems/flippingMatrix.py
@@ -0,0 +1,16 @@
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    n = len(matrix)
+    s = 0
+    for i in range(n//2):
+        for j in range(n//2):
+            s += max(matrix[i][j], matrix[i][n-j-1],
+                     matrix[n-i-1][n-j-1], matrix[n-i-1][j])
+    return s
