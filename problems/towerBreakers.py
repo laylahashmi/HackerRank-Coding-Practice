8f9e7eb90b60a6583c6bb343ee5f8b0406a6e1d3
Layla Hashmi <laylahashmi@gmail.com>
2023-08-06 17:46:20 -0500

    solved with o(n) complexity

---
+++ problems/towerBreakers.py
@@ -0,0 +1,9 @@
+# Two players are playing a game of Tower Breakers! Player  always moves first, and both players always play optimally.
+# The rules of the game are as follows:
+#
+# Initially there are n towers.
+# Each tower is of height m.
+# The players move in alternating turns.
+# In each turn, a player can choose a tower of height x and reduce its height to y, where  1 <= y < x and y evenly divides x.
+# If the current player is unable to make a move, they lose the game.
+# Given the values of  and , determine which player will win. If the first player wins, return 1. Otherwise, return 2.
